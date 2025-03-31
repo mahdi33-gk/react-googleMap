520b8df382310d15c57e0c0509aa597c6db1fbd6
mehedi <mehedi25122003@gmail.com>
2025-03-31 19:36:03 +0600

    api provider setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { APIProvider }
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <APIProvider>
-    <App />
+      <App />
     </APIProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
